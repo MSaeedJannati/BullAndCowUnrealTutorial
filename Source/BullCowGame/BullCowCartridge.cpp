bf314040f0d98c08c781d37a17ddd1d9a4564e1b
SaeedJannati <msaeedjannati72@gmail.com>
2022-08-05 03:40:05 +0430

    Twaeking level and adding welcome message

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-void UBullCowCartridge::BeginPlay() // When the game starts
+void UBullCowCartridge::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
+	PrintLine("Welcome to Bull Cows!");
+	PrintLine("Type something and press enter please.");
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString& Input)
 {
-
+	ClearScreen();
 }
