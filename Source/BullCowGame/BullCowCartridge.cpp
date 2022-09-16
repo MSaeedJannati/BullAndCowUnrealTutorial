d463887f446db9bb353375344faa3f3190d85a63
SaeedJannati <msaeedjannati72@gmail.com>
2022-09-16 17:41:41 +0430

    Completeing initialise game loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,16 +1,72 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+
 void UBullCowCartridge::BeginPlay()
 {
-	Super::BeginPlay();
-	PrintLine(TEXT("Welcome to Bull Cows!"));
-	PrintLine(TEXT("Type something and press enter please."));
+	InitialiseTheGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input)
 {
+	OnUserInput(_hiddenWord, Input);
+}
+
+void UBullCowCartridge::OnUserInput(const FString& hiddenWord, const FString& userInput)
+{
 	ClearScreen();
-	FString hiddenWord{TEXT("pube")};
-	PrintLine(Input);
+
+	if (CheckIfCorrectAnswer(hiddenWord, userInput))
+	{
+		OnCorrectAnswer();
+		return;
+	}
+	OnWrongAnswer();
+}
+
+FString UBullCowCartridge::ChooseRandomWord(const std::vector<FString>& choices)
+{
+	std::random_device randomDevice;
+	std::default_random_engine randomGenerator{randomDevice()};
+	const int32 max{static_cast<int32>(choices.size()) - 1};
+	std::uniform_int_distribution<int> dist{0, max};
+	const int32 randomIndex{dist(randomGenerator)};
+	return choices[randomIndex];
+}
+
+void UBullCowCartridge::PrintWelcomeMessages(const FString& hiddenWord)
+{
+	PrintLine(TEXT("Welcome to Bull Cows!"));
+	 int32 hiddenWordLength = _hiddenWord.Len();
+	TArray< FStringFormatArg > args; 
+	args.Add( FStringFormatArg( hiddenWordLength ) );
+	FString desctiprionMessage =FString::Format(TEXT("Guess the {0} letter word!"), args);
+	PrintLine(desctiprionMessage);
+	PrintLine(TEXT("press enter to continue..."));
+}
+
+bool UBullCowCartridge::CheckIfCorrectAnswer(const FString& hiddenWord, const FString& userInput)
+{
+	return hiddenWord == userInput;
+}
+
+void UBullCowCartridge::OnCorrectAnswer()
+{
+	PrintLine("Bulls eye!");
+}
+
+void UBullCowCartridge::OnWrongAnswer()
+{
+	PrintLine("Wrong answer baby!");
+}
+
+void UBullCowCartridge::InitialiseTheGame()
+{
+	Super::BeginPlay();
+	const std::vector<FString> choices{
+		TEXT("dick"),TEXT("pubes"),TEXT("arse"),TEXT("pussy"),TEXT("pimp"),TEXT("dildo")
+	};
+
+	_hiddenWord = ChooseRandomWord(choices);
+	PrintWelcomeMessages(_hiddenWord);
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -4,18 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "Console/Cartridge.h"
+#include <random>
+#include <string>
 #include "BullCowCartridge.generated.h"
+
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
-	public:
+public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
+	void OnUserInput(const FString& hiddenWord, const FString& userInput);
 
 	// Your declarations go below!
-	private:
-	
+private:
+#pragma region Fields
+FString _hiddenWord;
+#pragma  endregion
+#pragma  region Methods
+	FString ChooseRandomWord(const std::vector<FString>& choices);
+	void PrintWelcomeMessages(const FString& hiddenWord);
+	bool CheckIfCorrectAnswer(const FString& hiddenWord,const  FString& userInput);
+	void OnCorrectAnswer();
+	void OnWrongAnswer();
+void InitialiseTheGame();
+#pragma  endregion
 };
